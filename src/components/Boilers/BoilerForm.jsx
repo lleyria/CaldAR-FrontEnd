f177ba0204275fa459f2496636d4ebcedf5d57e8
Carla Baleani <carlabaleani27@gmail.com>
2020-12-23 00:37:06 -0300

    Change BoilerForm component to a functional one, using React Hooks

--- src/components/Boilers/BoilerForm.jsx
+++ src/components/Boilers/BoilerForm.jsx
@@ -1,42 +1,29 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import "./BoilerForm.css"
+import "./BoilerForm.css";
 
-class BoilerForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      id: Math.floor(Math.random() * 101),
-      lot: "",
-      companyName: "",
-      boilerType: "",
-      installationDate: "",
-      fabricationDate: "",
-      expirationDate: "",
-    };
+const BoilerForm = (props) => {
+  const [boiler, setBoiler] = useState(emptyBoiler);
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleEdit = this.handleEdit.bind(this);
-  }
-
-  componentDidMount() {
-    if (this.props.initialState) {
-      this.handleEdit(this.props.initialState);
+  useEffect(() => {
+    if (props.initialState) {
+      handleEdit(props.initialState);
+    } else {
+      setBoiler(emptyBoiler);
     }
-  }
+  }, [props.initialState]);
 
-  componentDidUpdate(prevProps) {
-    if (
-      this.props.isEditing &&
-      this.props.initialState !== prevProps.initialState
-    ) {
-      this.handleEdit(this.props.initialState);
-    }
-  }
+  const handleChange = (event) => {
+    setBoiler({ ...boiler, [event.target.name]: event.target.value });
+  };
 
-  handleEdit(boilerToEdit) {
-    this.setState({
+  const handleSubmit = (event) => {
+    props.onSubmit(boiler);
+    event.preventDefault();
+  };
+
+  const handleEdit = (boilerToEdit) => {
+    setBoiler({
       id: boilerToEdit.id,
       lot: boilerToEdit.lot,
       companyName: boilerToEdit.companyName,
@@ -45,97 +32,104 @@ class BoilerForm extends Component {
       fabricationDate: boilerToEdit.fabricationDate,
       expirationDate: boilerToEdit.expirationDate,
     });
-  }
+  };
 
-  handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
-  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="row">
+        <fieldset className="field-container">
+          <label>Lot</label>
+          <input
+            type="text"
+            name="lot"
+            placeholder="Lot"
+            value={boiler.lot}
+            onChange={handleChange}
+          />
+        </fieldset>
+        <fieldset className="field-container">
+          <label>Company Name</label>
+          <input
+            type="text"
+            name="companyName"
+            placeholder="Company Name"
+            value={boiler.companyName}
+            onChange={handleChange}
+          />
+        </fieldset>
+      </div>
+      <div className="row">
+        <fieldset className="field-container">
+          <label>Boiler Type</label>
+          <input
+            type="text"
+            name="boilerType"
+            placeholder="Boiler Type"
+            value={boiler.boilerType}
+            onChange={handleChange}
+          />
+        </fieldset>
+        <fieldset className="field-container">
+          <label>Installation Date</label>
+          <input
+            type="date"
+            name="installationDate"
+            placeholder="Installation Date"
+            value={boiler.installationDate.replace(
+              /(\d\d)\/(\d\d)\/(\d{4})/,
+              "$3-$1-$2"
+            )}
+            onChange={handleChange}
+          />
+        </fieldset>
+      </div>
+      <div className="row">
+        <fieldset className="field-container">
+          <label>Fabrication Date</label>
+          <input
+            type="date"
+            name="fabricationDate"
+            placeholder="Fabrication Date"
+            value={boiler.fabricationDate.replace(
+              /(\d\d)\/(\d\d)\/(\d{4})/,
+              "$3-$1-$2"
+            )}
+            onChange={handleChange}
+          />
+        </fieldset>
+        <fieldset className="field-container">
+          <label>Expiration Date</label>
+          <input
+            type="date"
+            name="expirationDate"
+            placeholder="Expiration Date"
+            value={boiler.expirationDate.replace(
+              /(\d\d)\/(\d\d)\/(\d{4})/,
+              "$3-$1-$2"
+            )}
+            onChange={handleChange}
+          />
+        </fieldset>
+      </div>
+      <button type="submit">Confirm</button>
+    </form>
+  );
+};
 
-  handleSubmit(event) {
-    this.props.onSubmit(this.state);
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <div className="row">
-          <fieldset className="field-container">
-            <label>Lot</label>
-            <input
-              type="text"
-              name="lot"
-              placeholder="Lot"
-              value={this.state.lot}
-              onChange={this.handleChange}
-            />
-          </fieldset>
-          <fieldset className="field-container">
-            <label>Company Name</label>
-            <input
-              type="text"
-              name="companyName"
-              placeholder="Company Name"
-              value={this.state.companyName}
-              onChange={this.handleChange}
-            />
-          </fieldset>
-        </div>
-        <div className="row">
-          <fieldset className="field-container">
-            <label>Boiler Type</label>
-            <input
-              type="text"
-              name="boilerType"
-              placeholder="Boiler Type"
-              value={this.state.boilerType}
-              onChange={this.handleChange}
-            />
-          </fieldset>
-          <fieldset className="field-container">
-            <label>Installation Date</label>
-            <input
-              type="date"
-              name="installationDate"
-              placeholder="Installation Date"
-              value={this.state.installationDate}
-              onChange={this.handleChange}
-            />
-          </fieldset>
-        </div>
-        <div className="row">
-          <fieldset className="field-container">
-            <label>Fabrication Date</label>
-            <input
-              type="date"
-              name="fabricationDate"
-              placeholder="Fabrication Date"
-              value={this.state.fabricationDate}
-              onChange={this.handleChange}
-            />
-          </fieldset>
-          <fieldset className="field-container">
-            <label>Expiration Date</label>
-            <input
-              type="date"
-              name="expirationDate"
-              placeholder="Expiration Date"
-              value={this.state.expirationDate}
-              onChange={this.handleChange}
-            />
-          </fieldset>
-        </div>
-        <button type="submit">Confirm</button>
-      </form>
-    );
-  }
-}
+const emptyBoiler = {
+  id: Math.floor(Math.random() * 101),
+  lot: "",
+  companyName: "",
+  boilerType: "",
+  installationDate: "",
+  fabricationDate: "",
+  expirationDate: "",
+};
 
 // PropTypes
 BoilerForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
-  initialState: PropTypes.object.isRequired,
-  isEditing: PropTypes.bool.isRequired,
+  initialState: PropTypes.object,
 };
 
 export default BoilerForm;
