6ae3d23c5ae97609bcee9da05602a1f70628d1de
Carla Baleani <carlabaleani27@gmail.com>
2020-12-27 13:06:45 -0300

    Redux initial set up

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { createStore } from "redux";
+import rootReducer from "./redux/reducers/rootReducer";
+import { Provider } from "react-redux";
+
+const configureStore = () => {
+  const enhancer =  composeWithDevTools();
+  return createStore(rootReducer, enhancer);
+}
+
+const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/redux/reducers/rootReducer.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+ // here the reducers
+});
+
+export default rootReducer;
