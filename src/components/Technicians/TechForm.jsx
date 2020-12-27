e31863a8cc838ca084d49473e7eb5430e80112db
Jose Luis Enriquez <josl.enriquez89@gmail.com>
2020-12-27 16:05:44 -0300

    Radium PS12 - Form create and style

--- src/components/Technicians/TechForm.jsx
+++ src/components/Technicians/TechForm.jsx
@@ -1,11 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import './TechForm.css';
 
 const TechForm = (props) => {
-    const [technician, setTechnician] = useState(initialFormState);
+    const [technician, setTechnician] = useState(emptyTechnician);
+
+    useEffect(() => {
+        if(props.initialState) {
+            handleEdit(props.initialState);
+        } else {
+            setTechnician(emptyTechnician);
+        }
+    }, [props.initialState]);
 
     const handleChange = (event) => {
-        setTechnician({ ...technician, [event.target.name]: event.target.value});
+        setTechnician({ ...technician, [event.target.name]: event.target.value });
     };
 
     const handleSubmit = (event) => {
@@ -13,84 +22,105 @@ const TechForm = (props) => {
         event.preventDefault();
     };
 
+    const handleEdit = (techToEdit) => {
+        setTechnician({
+            id: techToEdit.id,
+            firstName: techToEdit.firstName,
+            lastName: techToEdit.lastName,
+            email: techToEdit.email,
+            boilersType: techToEdit.boilersType,
+            professionalLevel: techToEdit.professionalLevel,
+            hourRate: techToEdit.hourRate,
+            monthlyCapacity: techToEdit.monthlyCapacity,
+        });
+    };
+
     return(
-        <form onSubmit = {handleSubmit}>
-            <fieldset>
-                <label>First Name</label>
-                <input
-                    type = 'text'
-                    name = 'firstName'
-                    placeholder = 'First Name'
-                    value = {technician.firstName}
-                    onChange = {handleChange}
-                />
-            </fieldset>
-            <fieldset>
-                <label>Last Name</label>
-                <input
-                    type = 'text'
-                    name = 'lastName'
-                    placeholder = 'Last Name'
-                    value = {technician.lastName}
-                    onChange = {handleChange}
-                />
-            </fieldset>
-            <fieldset>
-                <label>Email</label>
-                <input
-                    type = 'email'
-                    name = 'email'
-                    placeholder = 'Email'
-                    value = {technician.email}
-                    onChange = {handleChange}
-                />
-            </fieldset>
-            <fieldset>
-                <label>Boilers Type</label>
-                <input
-                    type = 'text'
-                    name = 'boilersType'
-                    placeholder = 'Boiler Type'
-                    value = {technician.boilersType}
-                    onChange = {handleChange}
-                />
-            </fieldset>
-            <fieldset>
-                <label>Professional Level</label>
-                <input
-                    type = 'text'
-                    name = 'professionalLevel'
-                    placeholder = 'Professional Level'
-                    value = {technician.professionalLevel}
-                    onChange = {handleChange}
-                />
-            </fieldset>
-            <fieldset>
-                <label>Hour Rate</label>
-                <input
-                    type = 'text'
-                    name = 'hourRate'
-                    placeholder = 'Hour Rate'
-                    value = {technician.hourRate}
-                    onChange = {handleChange}
-                />
-            </fieldset>
-            <fieldset>
-                <label>Monthly Capacity</label>
-                <input
-                    type = 'text'
-                    name = 'monthlyCapacity'
-                    placeholder = 'Monthly Capacity'
-                    value = {technician.monthlyCapacity}
-                    onChange = {handleChange}
-                />
-            </fieldset>
-            <button type="submit">Confirm</button>
+        <form onSubmit={handleSubmit}>
+            <div className = 'row'>
+                <fieldset className = 'field-container'>
+                    <label>First Name</label>
+                    <input
+                        type = 'text'
+                        name = 'firstName'
+                        placeholder = 'First Name'
+                        value = {technician.firstName}
+                        onChange = {handleChange}
+                    />
+                </fieldset>
+                <fieldset className = 'field-container'>
+                    <label>Last Name</label>
+                    <input
+                        type = 'text'
+                        name = 'lastName'
+                        placeholder = 'Last Name'
+                        value = {technician.lastName}
+                        onChange = {handleChange}
+                    />
+                </fieldset>
+            </div>
+            <div className = 'row'>
+                <fieldset className = 'field-container'>
+                    <label>Email</label>
+                    <input
+                        type = 'email'
+                        name = 'email'
+                        placeholder = 'Email'
+                        value = {technician.email}
+                        onChange = {handleChange}
+                    />
+                </fieldset>
+                <fieldset className = 'field-container'>
+                    <label>Boilers Type</label>
+                    <input
+                        type = 'text'
+                        name = 'boilersType'
+                        placeholder = 'Boilers Type'
+                        value = {technician.boilersType}
+                        onChange = {handleChange}
+                    />
+                </fieldset>
+            </div>
+            <div className = 'row'>
+                <fieldset className = 'field-container'>
+                    <label>Professional Level</label>
+                    <input
+                        type = 'text'
+                        name = 'professionalLevel'
+                        placeholder = 'Professional Level'
+                        value = {technician.professionalLevel}
+                        onChange = {handleChange}
+                    />
+                </fieldset>
+                <fieldset className = 'field-container'>
+                    <label>Hour Rate</label>
+                    <input
+                        type = 'text'
+                        name = 'hourRate'
+                        placeholder = 'Hour Rate'
+                        value = {technician.hourRate}
+                        onChange = {handleChange}
+                    />
+                </fieldset>
+            </div>
+            <div className = 'row'>
+                <fieldset className = 'field-container'>
+                    <label>Monthly Capacity</label>
+                    <input
+                        type = 'text'
+                        name = 'monthlyCapacity'
+                        placeholder = 'Monthly Capacity'
+                        value = {technician.monthlyCapacity}
+                        onChange = {handleChange}
+                    />
+                </fieldset>
+            </div>
+            <button type = 'submit'>Confirm</button>
         </form>
     );
 };
 
-const initialFormState = {
+const emptyTechnician = {
     id: Math.floor(Math.random() * 101),
     firstName: '',
     lastName: '',
@@ -101,8 +131,11 @@ const initialFormState = {
     monthlyCapacity: '',
 };
 
+// PropTypes
+
 TechForm.propTypes = {
-    onSubmit: PropTypes.function,
-}
+    onSubmit: PropTypes.func.isRequired,
+    initialState: PropTypes.object,
+};
 
 export default TechForm;
