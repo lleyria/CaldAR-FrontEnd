57aff884293825bc23ec3a3b1e5c0e12558d9328
Jose Luis Enriquez <josl.enriquez89@gmail.com>
2020-12-28 19:39:47 -0300

    Radium PS12 - Sidebar adapted to entities

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import SideBar from './Sidebar';
 import MainContainer from '../components/MainContainer';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Layout = ({children}) => {
     return(
@@ -10,22 +11,44 @@ const Layout = ({children}) => {
                 <SideBar width = {250} height = {'100vh'}>
                     <div className='caldar'>CaldAR</div>
                     <div>
-                        <i className = 'fas fa-laptop'></i> Administrators
+                        <Link to ='/'>
+                            <i className = 'fas fa-home'></i> Home
+                        </Link>
                     </div>
                     <div>
-                        <i className = 'fas fa-user-friends'></i> Clients
+                        <Link to = '/customers/buildings'>
+                            <i className = 'fas fa-building'></i> Buildings
+                        </Link>
                     </div>
                     <div>
-                        <i className = 'fas fa-tools'></i> Technicians
+                        <Link to = '/customers/companies'>
+                            <i className = 'fas fa-building'></i> Companies
+                        </Link>
                     </div>
                     <div>
-                        <i className = 'fas fa-building'></i> Boilers
+                        <Link to = '/technicians'>
+                            <i className = 'fas fa-tools'></i> Technicians
+                        </Link>
                     </div>
                     <div>
-                        <i className = 'fas fa-calendar'></i> Appoiments
+                        <Link to = '/boiler/boilers'>
+                            <i className = 'fab fa-hotjar'></i> Boilers
+                        </Link>
                     </div>
                     <div>
-                        <i className='fab fa-readme'></i> Reports
+                        <Link to = '/boiler/boilerstype'>
+                            <i className = 'fas fa-fire'></i> Boilers Type
+                        </Link>
+                    </div>
+                    <div>
+                        <Link to = '/appoiments'>
+                            <i className = 'fas fa-calendar'></i> Appoiments
+                        </Link>
+                    </div>
+                    <div>
+                        <Link to = '/reports'>
+                            <i className='fab fa-readme'></i> Reports
+                        </Link>
                     </div>
                 </SideBar>
             </div>
