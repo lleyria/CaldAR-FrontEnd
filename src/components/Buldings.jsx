96eeefc91b47f67203ee02d49dc801c6e5d04d26
Lisandro Leyria <l.leyria93@gmail.com>
2021-01-05 20:35:42 -0300

    Intial Commit

---
+++ src/components/Buldings.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {Form, Field} from 'react-final-form';
+
+class Buildings extends Component {
+    render(){
+        return(
+            <div>
+                This is Buildings
+            </div>
+        );
+    }
+}
+
+export default Buildings;
