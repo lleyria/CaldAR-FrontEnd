cab068f102f7067e79df5744629c04ce20ccfc06
Lisandro Leyria <l.leyria93@gmail.com>
2021-01-20 11:48:55 -0300

    Added Modal Logic

---
+++ src/modal/modal.js
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
+import style from "./modal.module.css";
+
+function Modal(props) {
+  const useStyles = makeStyles((theme) => ({
+    dialogWrapper: {
+      position: "absolute",
+      width: 800,
+      backgroundColor: theme.palette.background.paper,
+      border: "2px solid #000",
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
+
+  const { title, children, openModal, setOpenModal } = props;
+  const classes = useStyles();
+
+  return (
+    <Dialog
+      open={openModal}
+      modal={setOpenModal}
+      maxWidth="md"
+      classes={{ paper: classes.dialogWrapper }}
+    >
+      <DialogTitle className={style.title}>
+        <div style={{ display: "flex" }}>
+          <Typography variant="h6" component="div" style={{ flexGrow: 1 }}>
+            {title}
+          </Typography>
+        </div>
+      </DialogTitle>
+      <DialogContent dividers>
+        <div>{children}</div>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default Modal;
